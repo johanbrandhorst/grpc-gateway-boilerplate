85695a8c970bc36098029e2dfd9cbf699be1f8f6
Johan Brandhorst <johan.brandhorst@gmail.com>
2019-08-11 20:59:11 +0100

    Add field to AddUser

--- server/server.go
+++ server/server.go
@@ -22,12 +22,13 @@ func New() *Backend {
 }
 
 // AddUser adds a user to the in-memory store.
-func (b *Backend) AddUser(ctx context.Context, _ *usersv1.AddUserRequest) (*usersv1.AddUserResponse, error) {
+func (b *Backend) AddUser(ctx context.Context, req *usersv1.AddUserRequest) (*usersv1.AddUserResponse, error) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
 	user := &usersv1.User{
-		Id: uuid.Must(uuid.NewV4()).String(),
+		Id:    uuid.Must(uuid.NewV4()).String(),
+		Email: req.GetEmail(),
 	}
 	b.users = append(b.users, user)
 
